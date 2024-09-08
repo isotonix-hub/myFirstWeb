6ab3e2dc14d524b818ed353435f9a7f9f54fa34b
isotonix-hub <isotonix939@gmail.com>
2024-09-08 15:50:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public' , 'login.html'))
+    res.sendFile(path.join(__dirname, 'public' , 'index.html'))
 })
 
 // Create a MySQL connection
